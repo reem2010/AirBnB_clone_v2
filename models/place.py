ae06accf997bfcb28d15b47fbc0cdd9c63938109
Fadyy22 <fadyalaa441@gmail.com>
2023-09-17 23:47:12 +0300

    trying to fix

--- models/place.py
+++ models/place.py
@@ -4,18 +4,18 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Float, ForeignKey, String, Integer
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -11,7 +11,7 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship(
-        "Place", cascade='all, delete-orphan', backref="user")
+        "Place", cascade='all, delete, delete-orphan', backref="user")
