b086f751f3f758cd5617f93e3e3d2535289298cb
reem <tarekreem950@gmail.com>
2023-09-16 17:15:42 -0700

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER if not EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
