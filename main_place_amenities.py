b660982667b86f0b1710f61210ede7688dfa438c
reem <tarekreem950@gmail.com>
2023-09-18 04:18:16 -0700

    update state

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -18,34 +18,11 @@ state.save()
 city = City(state_id=state.id, name="San Francisco")
 city.save()
 
-# creation of a User
-user = User(email="john@snow.com", password="johnpwd")
-user.save()
+print(state.cities)
 
-# creation of 2 Places
-place_1 = Place(user_id=user.id, city_id=city.id, name="House 1")
-place_1.save()
-place_2 = Place(user_id=user.id, city_id=city.id, name="House 2")
-place_2.save()
 
-# creation of 3 various Amenity
-amenity_1 = Amenity(name="Wifi")
-amenity_1.save()
-amenity_2 = Amenity(name="Cable")
-amenity_2.save()
-amenity_3 = Amenity(name="Oven")
-amenity_3.save()
 
-# link place_1 with 2 amenities
-place_1.amenities.append(amenity_1)
-place_1.amenities.append(amenity_2)
-print(place_1.amenities)
 
-# link place_2 with 3 amenities
-place_2.amenities.append(amenity_1)
-place_2.amenities.append(amenity_2)
-place_2.amenities.append(amenity_3)
-print(amenity_1.place_amenities) 
 
 storage.save()
 print("OK")
--- models/state.py
+++ models/state.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
+import os
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship(
-            "City", backref="state", cascade="all, delete-orphan")
+    if os.environ.get("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship(
+                "City", backref="state", cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """returns the list of City instances"""
+            res = []
+            for value in models.storage.all(City).values():
+                if value.state_id == self.id:
+                    res.append(value)
+            return res
