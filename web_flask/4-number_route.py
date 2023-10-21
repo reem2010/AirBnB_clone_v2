49d54de1b8a59d72fa349d068f1f108110f207f8
reem <tarekreem950@gmail.com>
2023-10-21 06:47:01 -0700

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def route():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_lang(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def py_lang(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<n>")
+def py_lang(n):
+    if type(n) is int:
+        return f"{n} is  a number"
+    return
+
+
+if __name__ == '__main__':
+    app.run()
