8a37609ad342fc039170cb40afa0a31c85d049ca
reem <tarekreem950@gmail.com>
2023-10-24 19:37:01 -0700

    city

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(var):
+    storage.close()
+
+
+@app.route("/cities_by_states")
+def cities():
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
+
+
+if __name__ == '__main__':
+    app.run()
